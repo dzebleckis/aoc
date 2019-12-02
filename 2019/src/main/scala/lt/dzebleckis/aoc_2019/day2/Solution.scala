efe2badb610c06119f995e73fa007a18bec3ac0c
Darius Zebleckis <darius.zebleckis@lamoda.ru>
2019-12-03 00:15:11 +0200

    2019 day 2, stop searching when found

--- 2019/src/main/scala/lt/dzebleckis/aoc_2019/day2/Solution.scala
+++ 2019/src/main/scala/lt/dzebleckis/aoc_2019/day2/Solution.scala
@@ -4,8 +4,8 @@ import scala.io.Source
 object Solution {
   sealed trait Opcode
   final object Terminate extends Opcode
-  final object Add       extends Opcode
-  final object Multiply  extends Opcode
+  final object Add extends Opcode
+  final object Multiply extends Opcode
 
   def main(args: Array[String]): Unit = {
     val code = Source
@@ -21,29 +21,37 @@ object Solution {
     println(processCode(registry).head)
 
     val output = 19690720
-    for (noun <- 0 to 99) {
-      for (verb <- 0 to 99) {
+    var searching = true
+    var noun = 0
+
+    while (searching && noun <= 99) {
+      var verb = 0
+      while (searching && verb <= 99) {
         registry = code.toArray
         registry(1) = noun.toString
         registry(2) = verb.toString
 
         if (processCode(registry).head == output) {
           println(100 * noun + verb)
+          searching = false
         }
+        verb += 1
       }
+      noun += 1
     }
+
   }
 
   def processCode(code: Array[String]): Array[Int] = {
-    var running            = true
+    var running = true
     var instructionPointer = 0
-    val opcodes            = code.map(_.toInt)
+    val opcodes = code.map(_.toInt)
 
     while (running) {
       int2Opcode(opcodes(instructionPointer)) match {
         case Terminate => running = false
-        case Add       => execute(instructionPointer, opcodes, _ + _)
-        case Multiply  => execute(instructionPointer, opcodes, _ * _)
+        case Add => execute(instructionPointer, opcodes, _ + _)
+        case Multiply => execute(instructionPointer, opcodes, _ * _)
       }
       instructionPointer += 4
     }
@@ -55,8 +63,8 @@ object Solution {
       opcodes: Array[Int],
       op: (Int, Int) => Int
   ): Unit = {
-    val leftAddress   = opcodes(instructionPointer + 1)
-    val rightAddress  = opcodes(instructionPointer + 2)
+    val leftAddress = opcodes(instructionPointer + 1)
+    val rightAddress = opcodes(instructionPointer + 2)
     val resultAddress = opcodes(instructionPointer + 3)
 
     opcodes(resultAddress) = op(opcodes(leftAddress), opcodes(rightAddress))
@@ -64,8 +72,8 @@ object Solution {
 
   private def int2Opcode(i: Int): Opcode = i match {
     case 99 => Terminate
-    case 1  => Add
-    case 2  => Multiply
+    case 1 => Add
+    case 2 => Multiply
     case o =>
       throw new IllegalArgumentException(s"Operation not recognized $o")
   }
