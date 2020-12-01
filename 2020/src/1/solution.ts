b675222a6d24942567ee85e02f2e12ef23e6e183
Darius Zebleckis <darius.zebleckis@gmail.com>
2020-12-01 12:59:20 +0200

    Use Set instead of Map

--- 2020/src/1/solution.ts
+++ 2020/src/1/solution.ts
@@ -1,12 +1,12 @@
 const text = await Deno.readTextFile("./input");
 const lines = text.split("\n");
 
-let expenses = new Map();
+let expenses = new Set<number>();
 for (const l  of lines) {
-    expenses.set(Number(l), 1);
+    expenses.add(Number(l));
 }
 
-for (const e of expenses.keys()) {
+for (const e of expenses) {
     const o = 2020 - e;
     if (o > 0 && expenses.has(o)) {
         console.log(e, o, e * o);
